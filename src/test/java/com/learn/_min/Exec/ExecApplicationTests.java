875f840f52858e7c4028e134f01ac0a58ebbe089
Sarvanan N <nsaravanan1204@gmail.com>
2024-11-26 22:41:59 +0530

    Added unit test

--- src/test/java/com/learn/_min/Exec/ExecApplicationTests.java
+++ src/test/java/com/learn/_min/Exec/ExecApplicationTests.java
@@ -8,6 +8,7 @@ class ExecApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
--- src/test/java/juint/MyMathTest.java
+++ src/test/java/juint/MyMathTest.java
@@ -3,11 +3,17 @@ package juint;
 import org.junit.jupiter.api.Test;
 
 import static com.jayway.jsonpath.internal.path.PathCompiler.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MyMathTest {
 
     @Test
     void test(){
         //Absence of failure is success
+        int [] num = {1,2,3};
+        MyMath mymath = new MyMath();
+        int result = mymath.calculateSum(num);
+        int expectedResult = 5;
+        assertEquals(expectedResult,result);
     }
 }
