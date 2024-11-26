3de8aa94daed3b36dd2bc53381c1a941f0119e8c
Sarvanan N <nsaravanan1204@gmail.com>
2024-11-26 22:36:52 +0530

    Added Simple Junit

---
+++ src/test/java/juint/MyMathTest.java
@@ -0,0 +1,13 @@
+package juint;
+
+import org.junit.jupiter.api.Test;
+
+import static com.jayway.jsonpath.internal.path.PathCompiler.fail;
+
+public class MyMathTest {
+
+    @Test
+    void test(){
+        //Absence of failure is success
+    }
+}
