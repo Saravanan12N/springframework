f095a95a98f01a08847851f3db7c81f75d82b045
Sarvanan N <nsaravanan1204@gmail.com>
2024-11-27 18:47:44 +0530

    Added More Test Cases

--- src/test/java/juint/MyMathTest.java
+++ src/test/java/juint/MyMathTest.java
@@ -1,13 +1,16 @@
 package juint;
-
 import org.junit.jupiter.api.Test;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import static com.jayway.jsonpath.internal.path.PathCompiler.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MyMathTest {
 
     MyMath mymath = new MyMath();
+    List<String> asList  = Arrays.asList("AWS","AZURE","GCP");
 
     @Test
     void calculateThreeNumbers(){
@@ -20,4 +23,14 @@ public class MyMathTest {
         //Absence of failure is success
         assertEquals(0, mymath.calculateSum(new int[]{}));
     }
+
+    @Test
+    void testList(){
+        boolean test = asList.contains("AWS");
+        assertEquals(true,test);
+        assertEquals(3,asList.size());
+        assertTrue(test);
+    }
+
+
 }
